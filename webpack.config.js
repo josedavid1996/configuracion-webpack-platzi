1520e86b35304be59354e3086a37c2fd39198f29
Jose david Redondo <redondoperdomojosedavid@gmail.com>
2022-03-30 11:50:01 -0500

    Cuarta configuracion css y sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -19,6 +20,11 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.css|.sass|.scss$/i,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
     ],
   },
   plugins: [
@@ -27,5 +33,6 @@ module.exports = {
       template: "./public/index.html",
       filename: "./index.html",
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
